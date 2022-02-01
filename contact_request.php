05cb0547d1e624538c6f727d87d6eafd798188e7
Michael Hefner <michael.hefner@outlook.com>
2022-01-31 21:33:23 -0500

    email mods

--- contact_request.php
+++ contact_request.php
@@ -7,13 +7,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $reason = isset($_POST['reason']) ? $_POST['reason'] : '';
     echo($name . ' ' . $email);
     
-    $header = "From:abc@somedomain.com \r\n";
-    $header .= "Cc:afgh@somedomain.com \r\n";
+    $header = "From:". $email . " \r\n";
     $header .= "MIME-Version: 1.0\r\n";
     $header .= "Content-type: text/html\r\n";
     if(mail( 'ryan@ryansmeat.com', 'ryansmeat lead', $name, $header ) == true){
         echo('<h1>Email successfully sent</h1>');
-        sleep(1);
         header('Location: '.'/');
     } else {
         echo('error');
